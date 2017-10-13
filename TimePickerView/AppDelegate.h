7a1768e6e5c339446df6c698dcbc58214d3ed21a
gz <gaoze0506@163.com>
2017-10-13 20:58:59 +0800

    Initial Commit

---
+++ TimePickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimePickerView
+//
+//  Created by gz on 2017/10/13.
+//  Copyright © 2017年 gz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimePickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimePickerView
+//
+//  Created by gz on 2017/10/13.
+//  Copyright © 2017年 gz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
